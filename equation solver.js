09e0e2a951c02d13662d7ac39dfba0a655c38e8b
hiraj <hirajsumit@gmail.com>
2020-10-01 17:34:28 +0530

    Adding equation solver made in Javascript

---
+++ equation solver.js
@@ -0,0 +1,7 @@
+//https://www.facebook.com/vtusolution/posts/923367628154366
+//subscribed by sumit raj
+
+console.log("This is a calculator which can solve equation"); // welcome message
+var inputdata = prompt("Please give equation "); // asking for input
+let result = eval(inputdata); // calculating result
+console.log(result); //showing output
