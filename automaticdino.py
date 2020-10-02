2900673517b3435615a9de256b8b37134b84dc6e
Chirag Badhe <badhechirag633@gmail.com>
2020-10-02 12:33:38 -0700

    Create automaticdino.py

---
+++ automaticdino.py
@@ -0,0 +1,52 @@
+#code by @chirag Badhe
+
+import pyautogui # pip install pyautogui
+from PIL import Image, ImageGrab # pip install pillow
+# from numpy import asarray
+import time
+
+def hit(key):
+    pyautogui.keyDown(key)
+    return
+
+def isCollide(data):
+    # Draw the rectangle for birds
+    for i in range(350, 400):
+        for j in range(200, 300):
+            if data[i, j] < 100:
+                hit("down")
+                return
+
+    for i in range(300, 350):
+        for j in range(300, 350):
+            if data[i, j] < 100:
+                hit("up")
+                return
+    return
+
+if __name__ == "__main__":
+    print("Hey.. Dino game about to start in 3 seconds")
+    time.sleep(2)
+    # hit('up') 
+
+    while True:
+        image = ImageGrab.grab().convert('L')  
+        data = image.load()
+        isCollide(data)
+            
+        # print(asarray(image))
+        
+    #   # Draw the rectangle for cactus
+    #     for i in range(300, 350):
+    #         for j in range(300, 350):
+    #             data[i, j] = 0
+        
+    #     # Draw the rectangle for birds
+    #     for i in range(350, 400):
+    #         for j in range(200, 300):
+    #             data[i, j] = 171
+
+    #     image.show()
+    #     break
+    
+      
