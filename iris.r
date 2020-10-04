36cc7d3bb0d92c2f66e9b4117494fbb8af13ce89
AnandaKrishnanNair <65528160+AnandaKrishnanNair@users.noreply.github.com>
2020-10-04 19:55:14 +0530

    added iris data set

---
+++ iris.r
@@ -0,0 +1,25 @@
+
+iris
+a=iris$Sepal.Length
+b=iris$Petal.Length
+# from  here sepal length variables began
+mean(a)
+median(a)
+max(a)-min(a)
+quantile(a)
+quantile(a,c(.18,.53,.95))
+var(a)
+sd(a)
+# from here petal length variables began
+mean(b)
+median(b)
+max(b)-min(b)
+quantile(b)
+quantile(b,c(.18,.53,.95))
+var(b)
+sd(b)
+cov(a,b)
+cor(a,b)
+
+
+
